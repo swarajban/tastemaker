9f21ebd14295ed3fb9649bcd70ef05c5fb750c4d
swa <swaraj@semprehealth.com>
2024-12-31 14:58:26 -0500

    Add signup ability

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -18,6 +18,7 @@ export default function LoginPage() {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [emailSent, setEmailSent] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
+  const [isNewUser, setIsNewUser] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -36,41 +37,59 @@ export default function LoginPage() {
 
   return (
     <Box p={8} maxWidth="400px" margin="0 auto">
-      <Heading mb={4}>Login</Heading>
+      <Heading mb={4}>{isNewUser ? 'Sign Up' : 'Login'}</Heading>
       
       {emailSent ? (
         <Alert status="success" mb={4}>
           <AlertIcon />
-          Check your email for the login link!
+          Check your email for the {isNewUser ? 'signup' : 'login'} link!
         </Alert>
       ) : (
-        <form onSubmit={handleSubmit}>
-          <FormControl mb={4}>
-            <FormLabel>Email</FormLabel>
-            <Input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </FormControl>
+        <>
+          <Text mb={4}>
+            {isNewUser 
+              ? 'Enter your email to create an account'
+              : 'Enter your email to login to your account'}
+          </Text>
+          <form onSubmit={handleSubmit}>
+            <FormControl mb={4}>
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </FormControl>
 
-          {errorMsg && (
-            <Text color="red.500" mb={2}>
-              {errorMsg}
-            </Text>
-          )}
+            {errorMsg && (
+              <Text color="red.500" mb={2}>
+                {errorMsg}
+              </Text>
+            )}
 
-          <Button 
-            type="submit" 
-            colorScheme="teal" 
-            width="100%"
-            isLoading={isSubmitting}
-            loadingText="Sending..."
-          >
-            Send Login Link
-          </Button>
-        </form>
+            <Button 
+              type="submit" 
+              colorScheme="teal" 
+              width="100%"
+              isLoading={isSubmitting}
+              loadingText="Sending..."
+            >
+              {isNewUser ? 'Sign Up' : 'Login'}
+            </Button>
+
+            <Button
+              variant="link"
+              width="100%"
+              mt={4}
+              onClick={() => setIsNewUser(!isNewUser)}
+            >
+              {isNewUser 
+                ? 'Already have an account? Login'
+                : "Don't have an account? Sign Up"}
+            </Button>
+          </form>
+        </>
       )}
     </Box>
   );
